af6585ec7cf244f04b2b76d0fca03e3e9d4299d0
Johnathon Wood <jaywood128@gmail.com>
2018-10-04 15:01:18 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,12 +12,9 @@ function frameIt() {
 function submitIt() {
   $('form').on("submit", function() {
   if ($("input:first").val() === "correct") {
-    alert('your form is going to be submitted now')
+    alert('Your form is going to be submitted now.')
     return
-  }
-    alert("you entered the wrong value")
-    return
-
+  }  
 })//define functions here
 $(document).ready(function(){
 
