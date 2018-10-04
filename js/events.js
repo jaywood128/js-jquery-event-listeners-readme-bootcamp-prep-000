c62aabd12d918de5c65df54d190f629d00d18473
Johnathon Wood <jaywood128@gmail.com>
2018-10-04 15:41:04 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -19,7 +19,7 @@ function pressIt() {
   $(document).on('keydown', function(key)
 {
   if(key.which == 71)
-   alert('g was pressed')
+   alert('G was pressed')
  }
 }) //define functions here
 $(document).ready(function(){
