b4129fad93f5519d218a77f8a51b0659232f2995
Johnathon Wood <jaywood128@gmail.com>
2018-10-04 15:01:45 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function frameIt() {
 function submitIt() {
   $('form').on("submit", function() {
   if ($("input:first").val() === "correct") {
-    alert('Your form is going to be submitted now.')
+    alert("Your form is going to be submitted now.")
       return
   }
 })//define functions here
