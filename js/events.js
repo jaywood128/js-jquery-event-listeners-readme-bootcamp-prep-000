a2843b62e06dd36cd69c5ea0b9d67c38bd9c1faa
Johnathon Wood <jaywood128@gmail.com>
2018-10-04 14:38:13 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -8,9 +8,18 @@ function frameIt() {
     $('img').addClass('tasty')
 
   })
+}
+function submit() { 
+  $('form').on("submit", function() {
+  if ($("input:first").val() ==="correct") {
+    alret('your form is going to be submitted now')
+    return
+  })
+  
 }//define functions here
 $(document).ready(function(){
 
  //getIt();// call functions here
   frameIt();
+  submit();
 });
